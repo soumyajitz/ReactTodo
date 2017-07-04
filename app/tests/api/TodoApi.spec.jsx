45f4383f62b91a9dda264951ff4c29df203f49b3
soumyajitz <2014soumyajit@gmail.com>
2017-07-04 16:55:20 -0500

    feat(add-search): wired up search and it tests

--- app/tests/api/TodoApi.spec.jsx
+++ app/tests/api/TodoApi.spec.jsx
@@ -51,6 +51,49 @@ describe('TodoApi', () => {
             var actualTodos = TodoApi.getTodos();
 
             expect(actualTodos).toEqual(todos);
-        })
+        });
+
+        describe('filterTodos', () => {
+            var todos = [{
+                id: 1,
+                text: 'some Text 1',
+                completed: true
+            },
+            {
+                id: 2,
+                text: 'Other Text 2',
+                completed: false
+            },
+            {
+                id: 3,
+                text: 'some Text 3',
+                completed: true
+            }];
+
+            it('should return all items if showCompleted is true', () => {
+                var filteredTodos = TodoApi.filterTodos(todos, true, '');
+                expect(filteredTodos.length).toBe(3);
+            });
+
+             it('should return all items if showCompleted is false', () => {
+                var filteredTodos = TodoApi.filterTodos(todos, false, '');
+                expect(filteredTodos.length).toBe(1);
+            });
+
+            it('should sort by completed status', () => {
+                var filteredTodos = TodoApi.getTodos(todos, true, '');
+                expect(filteredTodos[0].completed).toBe(false);
+            });
+
+            it('should return filter todos by search text', () => {
+                var filteredTodos = TodoApi.filterTodos(todos, true, 'some');
+                expect(filteredTodos.length).toBe(2);
+            });
+
+             it('should return all todos if search text is empty', () => {
+                var filteredTodos = TodoApi.filterTodos(todos, true, '');
+                expect(filteredTodos.length).toBe(3);
+            });
+        });
     })
 })
